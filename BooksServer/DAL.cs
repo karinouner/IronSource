ce9a8ae21f9a1fabba45e57cd89e32f83fb62ecd
karinouner <karinouner@gmail.com>
2019-01-21 20:54:11 +0200

    Server

---
+++ BooksServer/DAL.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace BooksServer
+{
+    public class DAL
+    {
+        readonly string con;
+        public DAL()
+        {
+            //con = ConfigurationManager.ConnectionStrings["db"].ConnectionString;
+            con = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Books;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        }
+        
+
+        public IEnumerable<dynamic> RunQuery(string query)
+        {
+            using (var connection = new SqlConnection(con))
+            {
+                var result = connection.Query(query);
+                return result;
+            }
+        }
+    }
+}
---
+++ BooksServer/DALQueries.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksServer
+{
+    public class DALQueries
+    {
+        public enum QueryType
+        {
+            GetUser,
+            GetAllBooks,
+            GetBooksById,
+            AddBookToUser,
+            ReturnFromBorrow
+        }
+
+        public static Dictionary<QueryType, string> queriesDict = new Dictionary<QueryType, string>
+        {
+            { QueryType.GetUser , "SELECT * FROM users where idnumber = "},
+            { QueryType.GetAllBooks , "SELECT * FROM Books"},
+            { QueryType.GetBooksById, @" SELECT Books.Id, Books.Name , Books.Author , Books.Genre , Books.PublishYear
+                                         FROM Map_Books_Users
+                                         INNER JOIN Books on books.Id = Map_Books_Users.BookId
+                                         WHERE IdNumber = " },
+            { QueryType.AddBookToUser, "INSERT INTO Map_Books_Users " },
+            { QueryType.ReturnFromBorrow, "DELETE FROM Map_Books_Users WHERE " }
+        };
+    }
+}
